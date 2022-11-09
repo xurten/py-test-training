6be03a38ea543750b0f2e5889e409d2eaaef291c
Andrzej <xurten@gmail.com>
2022-11-09 10:23:48 +0100

    Update

--- pages/search.py
+++ pages/search.py
@@ -1,3 +1,5 @@
+from concurrent.futures._base import LOGGER
+
 from playwright.sync_api import Page
 
 
@@ -6,12 +8,16 @@ class DuckDuckGoSearchPage:
 
     def __init__(self, page: Page) -> None:
         self.page = page
-        self.search_button = page.locator('#search_button_homepage')
-        self.search_input = page.locator('#search_form_input_homepage')
+        self.search_button = page.get_by_role("button", name="Search")
+        self.search_input = page.locator('//*[@id="searchbox_input"]')
+        print("Init")
 
     def load(self) -> None:
         self.page.goto(self.URL)
+        print("Load")
+
 
     def search(self, phrase: str) -> None:
         self.search_input.fill(phrase)
         self.search_button.click()
+        print("Search")
--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -2,36 +2,47 @@ import re
 
 import pytest
 from playwright.sync_api import Page, expect
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 
+ANIMALS = [
+    'panda',
+    'python'
+]
+# ANIMALS = [
+#     'panda',
+#     'python',
+#     'polarbear',
+#     'parrot',
+#     'porcupine',
+#     'parakeet',
+#     'pangolin',
+#     'panther',
+#     'platypus',
+#     'peacock'
+# ]
 
 @pytest.fixture(scope="function", autouse=True)
 def before_each_after_each(page: Page):
-    # browser = chromium.launch()
-    # context = browser.new_context()
-    # Start tracing before creating / navigating a page.
     page.context.tracing.start(screenshots=True, snapshots=True, sources=True)
     print("beforeEach")
 
     yield
     print("afterEach")
     page.context.tracing.stop(path="trace.zip")
+    page.close()
 
 
-def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
+def test_homepage_has_playwright(page: Page):
     page.goto("https://playwright.dev/")
-
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
-
     # create a locator
     get_started = page.get_by_role("link", name="Get started");
-
     # Expect an attribute "to be strictly equal" to the value.
     expect(get_started).to_have_attribute("href", "/docs/intro")
-
     # Click the get started link.
     get_started.click()
-
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*intro"))
     footer = page.locator(".footer__copyright")
@@ -39,3 +50,18 @@ def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_in
     assert text[0] == "Copyright © 2022 Microsoft"
 
 
+@pytest.mark.parametrize('phrase', ANIMALS)
+def test_homepage(phrase: str, page: Page, search_page: DuckDuckGoSearchPage,
+                  result_page: DuckDuckGoResultPage) -> None:
+    search_page = DuckDuckGoSearchPage(page)
+    result_page = DuckDuckGoResultPage(page)
+    # Given the DuckDuckGo home page is displayed
+    search_page.load()
+    # When the user searches for a phrase
+    search_page.search(phrase)
+    # Then the search result query is the phrase
+    expect(result_page.search_input).to_have_value(phrase)
+    # # And the search result links pertain to the phrase
+    assert result_page.result_link_titles_contain_phrase(phrase)
+    # # And the search result title contains the phrase
+    expect(page).to_have_title(f'{phrase} at DuckDuckGo')
