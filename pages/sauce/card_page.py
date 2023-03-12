dac0e0860c8f288c6d648c21f601b6b889f2ada7
Andrzej <xurten@gmail.com>
2023-03-12 17:47:28 +0100

    Refactor card page

--- pages/sauce/card_page.py
+++ pages/sauce/card_page.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional
 from playwright.sync_api import Page
 from library.helper import sum_list_of_strings
 from pages.sauce.base_page import BasePage
@@ -9,32 +9,24 @@ class CardPage(BasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
 
-    def _get_card_quantity_in_menu(self) -> Optional[str]:
-        return self.page \
-            .locator('//*[@id="shopping_cart_container"]/a/span') \
-            .text_content()
+    def menu_quantity(self) -> Optional[str]:
+        return self.page.locator('//*[@id="shopping_cart_container"]/a/span').text_content()
 
-    def _get_card_quantity(self) -> str:
-        card_quantity = self.page \
-            .locator('.cart_quantity') \
-            .all_text_contents()
-        return sum_list_of_strings(card_quantity)
+    def card_quantity(self) -> str:
+        quantities = self.page.query_selector_all('.cart_quantity')
+        return sum_list_of_strings([q.text_content() for q in quantities])
 
     def verify_card_quantity(self, expected_card_quantity):
-        card_menu_quantity = self._get_card_quantity_in_menu()
-        card_quantity = self._get_card_quantity()
+        card_menu_quantity = self.menu_quantity()
+        card_quantity = self.card_quantity()
         assert card_menu_quantity == expected_card_quantity, f" Wrong menu card quantity : {card_menu_quantity} != {expected_card_quantity}"
         assert card_quantity == expected_card_quantity, f" Wrong card quantity : {card_quantity} != {expected_card_quantity}"
         return self
 
     def click_checkout(self) -> None:
-        self.page \
-            .locator('.checkout_button') \
-            .click()
+        self.page.locator('.checkout_button').click()
         return self
 
     def click_continue_shopping(self) -> None:
-        self.page \
-            .locator('#continue-shopping') \
-            .click()
+        self.page.locator('#continue-shopping').click()
         return self
