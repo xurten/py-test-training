c383775e87f4282778f5a25ab8959b7f83c7b9bc
Andrzej <xurten@gmail.com>
2023-03-12 17:04:12 +0100

    Refactor for login page

--- pages/sauce/login_page.py
+++ pages/sauce/login_page.py
@@ -12,33 +12,32 @@ class LoginPage(BasePage):
         self.username = page.locator("[data-test=\"username\"]")
         self.password = page.locator("[data-test=\"password\"]")
         self.login_button = page.locator("[data-test=\"login-button\"]")
+        self.error_message = page.locator("[data-test=\"error\"]")
 
-    def login_as_user(self, username: str, password: str) -> None:
+    def login_as_user(self, username: str, password: str) -> 'LoginPage':
         self.page.goto(self.URL)
+        self.page.expect_navigation()
         self.set_credentials(username, password)
         self.click_login()
         return self
 
     def _get_error_message(self) -> str:
-        self.page.locator("[data-test=\"error\"]")\
-            .text_content()
-        return remove_html_tags_from_string(self.page
-                                            .locator("[data-test=\"error\"]")
-                                            .inner_html())
+        self.error_message.text_content()
+        return remove_html_tags_from_string(self.error_message.inner_html())
 
-    def verify_url(self, expected_url):
+    def verify_url(self, expected_url) -> 'LoginPage':
         assert self.page.url == expected_url
         return self
 
-    def verify_error_message(self, expected_error_message):
+    def verify_error_message(self, expected_error_message) -> None:
         assert self._get_error_message() == expected_error_message
 
-    def set_credentials(self, username: str, password: str) -> None:
+    def set_credentials(self, username: str, password: str) -> 'LoginPage':
         self.username.fill(username)
         self.password.fill(password)
         return self
 
-    def click_login(self) -> None:
+    def click_login(self) -> 'LoginPage':
         self.login_button.click()
         return self
 
